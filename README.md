c74de6a3d7a35aaadb98caa96245d1189a396b76
rice <ricemarch@foxmail.com>
2021-09-17 17:32:36 +0800

    JsonElement; JsonPrimitive; LazilyParsedNumber;

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
-Gson is a Java library that can be used to convert Java Objects into their
-JSON representation. It can also be used to convert a JSON string to an
-equivalent Java object. Gson can work with arbitrary Java objects including
-pre-existing objects that you do not have source-code of.
+Gson is a Java library that can be used to convert Java Objects into their JSON representation. It can also be used to
+convert a JSON string to an equivalent Java object. Gson can work with arbitrary Java objects including pre-existing
+objects that you do not have source-code of.
 
 Complete Gson documentation is available at its project page
 https://github.com/google/gson
 
-
 Through the study of Gson source code to establish this project
+# internal
+
+```
+|-- $Gson$Preconditions
+|-- LazilyParsedNumber
+|-- LinkedTreeMap
+|-- Primitives
+```
+
+
+# Object
+
+```
+|JsonElement
+|-- JsonArray
+|-- JsonNull
+|-- JsonObject
+|-- JsonPritive
+```
+
+# Exception
+
+```
+|ParseException
+|-- IOException
+|-- SyntaxException
+```
--- src/com/ricemarch/gson/JsonPrimitive.java
+++ src/com/ricemarch/gson/JsonPrimitive.java
@@ -1,6 +1,7 @@
 package com.ricemarch.gson;
 
 import com.ricemarch.gson.internal.$Gson$Preconditions;
+import com.ricemarch.gson.internal.LazilyParsedNumber;
 
 /**
  * A Class representing a Json primitive value. A primitive value
@@ -59,5 +60,57 @@ public class JsonPrimitive extends JsonElement {
         value = $Gson$Preconditions.checkNotNull(character).toString();
     }
 
+
+    /**
+     * Check whether this primitive contains a boolean value.
+     *
+     * @return true if this primitive contains a boolean value, false otherwise.
+     */
+    public boolean isBoolean() {
+        return value instanceof Boolean;
+    }
+
+    /**
+     * convenience method to get this element as a boolean value.
+     *
+     * @return get this element as a primitive boolean value.
+     */
+    @Override
+    public boolean getAsBoolean() {
+        if (isBoolean()) {
+            return ((Boolean) value).booleanValue();
+        }
+        // Check to see if the value as a String is "true" in any case.
+        return Boolean.parseBoolean(getAsString());
+    }
+
+    /**
+     * Check whether this primitive contains a Number.
+     *
+     * @return true if this primitive contains a Number, false otherwise.
+     */
+    public boolean isNumber() {
+        return value instanceof Number;
+    }
+
+    @Override
+    public Number getAsNumber() {
+        return value instanceof String ? new LazilyParsedNumber((String) value) : (Number) value;
+    }
+
+    public boolean isString() {
+        return value instanceof String;
+    }
+
+    @Override
+    public String getAsString() {
+        if (isNumber()) {
+            return getAsNumber().toString();
+        } else if (isBoolean()) {
+            return ((Boolean) value).toString();
+        } else {
+            return (String) value;
+        }
+    }
 
 }
